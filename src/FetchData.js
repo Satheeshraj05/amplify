e684c5b7d7b2944989756c9e843d7531ce499957
Satheesh <57912584+Satheeshraj05@users.noreply.github.com>
2024-09-20 12:17:35 +0530

    FetchData.js

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'; // Import App.css if needed
+import './App.css'; 
 
 const FetchData = () => {
     const [data, setData] = useState([]);
