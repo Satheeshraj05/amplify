ad8e98f975156b16a22f8ff72152b78c26e3323b
Satheesh <57912584+Satheeshraj05@users.noreply.github.com>
2024-09-20 12:18:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';  // Import createRoot from react-dom/client
+import ReactDOM from 'react-dom/client';  
 import App from './App';
 import './index.css';
-import { Amplify } from 'aws-amplify'; // Import Amplify as a named import
+import { Amplify } from 'aws-amplify'; 
 import awsconfig from './aws-exports';
 
 // Configure Amplify with the configuration from aws-exports
 Amplify.configure(awsconfig);
 
-// Create a root and render the App component
-const root = ReactDOM.createRoot(document.getElementById('root'));  // Create a root
+const root = ReactDOM.createRoot(document.getElementById('root'));  
 root.render(
   <React.StrictMode>
     <App />
